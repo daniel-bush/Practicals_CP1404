158596d94739e3dadf17ea9a3a926aa32c524606
daniel.bush <Themepw999GH1987>
2020-08-26 16:07:23 +0100

    add function descriptions

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -4,13 +4,15 @@ def main():
 
 
 def collect_numbers() -> list:
+    """Collect numbers as integers from user and adds them to a list. The list is returned."""
     numbers = []
     for i in range(5):
         numbers.append(int(input("Number: ")))
     return numbers
 
 
-def calculate_stats(number_list: list):
+def calculate_stats(number_list: list) -> None:
+    """Print statistics to the screen based on the number list that it is passed."""
     print("The first number is {}".format(number_list[0]))
     print("The last number is {}".format(number_list[-1]))
     print("The smallest number is {}".format(min(number_list)))
