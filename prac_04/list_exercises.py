f98f1a9cd4404b990f12f66745755a844a1bc79a
daniel.bush <Themepw999GH1987>
2020-08-26 16:04:38 +0100

    add calculate_stats function - task complete

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -2,15 +2,21 @@ def main():
     numbers = collect_numbers()
     calculate_stats(numbers)
 
+
 def collect_numbers() -> list:
     numbers = []
     for i in range(5):
-        numbers.append(input("Number: "))
+        numbers.append(int(input("Number: ")))
     return numbers
 
 
 def calculate_stats(number_list: list):
-
+    print("The first number is {}".format(number_list[0]))
+    print("The last number is {}".format(number_list[-1]))
+    print("The smallest number is {}".format(min(number_list)))
+    print("The largest number is {}".format(max(number_list)))
+    average = sum(number_list) / len(number_list)
+    print("The average of the numbers is {}".format(average))
 
 
 main()
