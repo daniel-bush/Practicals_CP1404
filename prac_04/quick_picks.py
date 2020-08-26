880ff1666f6c6944ef97f6b9d12e55daaf375cab
daniel.bush <Themepw999GH1987>
2020-08-26 16:41:57 +0100

    add random number_selection function

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -7,4 +7,23 @@ Each line (quick pick) should not contain any repeated number.
 
 Each line of numbers should be displayed in sorted (ascending) order.
 """
+from random import randint
 
+NUMBERS_PER_LINE = 6
+LOWEST_VALUE = 1
+HIGHEST_VALUE = 45
+
+def main():
+    quick_picks = int(input("How many quick picks? "))
+
+
+def number_selection() -> list:
+    line = []
+    for i in NUMBERS_PER_LINE:
+        number = randint(LOWEST_VALUE, HIGHEST_VALUE)
+        line.append(number)
+    return line
+
+
+
+main()
