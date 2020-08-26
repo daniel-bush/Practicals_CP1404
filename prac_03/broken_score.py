59dff35917838f1fb83549d86d705e382e634c25
daniel.bush <Themepw999GH1987>
2020-08-26 10:33:56 +0100

    add random score generation to main

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,10 +3,13 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+import random
+
 
 def main():
     score = float(input("Enter score: "))
     print(get_score(score))
+    print(get_score(random.randint(0, 100)))
 
 
 def get_score(score):
