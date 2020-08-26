f540439f93754e8c77b72af0122d2c1b2b4bae7b
daniel.bush <Themepw999GH1987>
2020-08-26 15:37:20 +0100

    Add subject_details printing function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,7 +8,13 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    subject_details(data)
+
+
+def subject_details(data):
+    """Takes data in list form and prints it into English on the screen."""
+    for line in data:
+        print("{} is taught by {} and has {} students".format(line[0], line[1], line[2]))
 
 
 def get_data():
@@ -16,18 +22,17 @@ def get_data():
     master_list = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        # print("----------")
         master_list.append(parts)
     input_file.close()
     return master_list
-
 
 
 main()
