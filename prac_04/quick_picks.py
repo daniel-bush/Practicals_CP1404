ed40371b11f2d521194c60e5fe966027fcac407b
daniel.bush <Themepw999GH1987>
2020-08-27 08:18:16 +0100

    update to number_selection function. Add unique value checker.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -13,15 +13,27 @@ NUMBERS_PER_LINE = 6
 LOWEST_VALUE = 1
 HIGHEST_VALUE = 45
 
+
 def main():
-    quick_picks = int(input("How many quick picks? "))
+    no_of_lines = int(input("How many quick picks? "))
+    line_values = []
+    for i in range(no_of_lines):
+        line = number_selection()
+        line_values.append(line)
+    print(line_values)
 
 
 def number_selection() -> list:
+    """Generates list of unique random integers within a specified range. Returns list."""
     line = []
-    for i in NUMBERS_PER_LINE:
-        number = randint(LOWEST_VALUE, HIGHEST_VALUE)
-        line.append(number)
+    for i in range(NUMBERS_PER_LINE):
+        not_unique = True
+        while not_unique:
+            number = randint(LOWEST_VALUE, HIGHEST_VALUE)
+            if number not in line:
+                line.append(number)
+                not_unique = False
+    line.sort()
     return line
 
 
