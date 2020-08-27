b914e60a86ef8d73a48e6fb8e29df7ad943f5b22
daniel.bush <Themepw999GH1987>
2020-08-27 08:24:32 +0100

    update function and variable names.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -17,22 +17,24 @@ HIGHEST_VALUE = 45
 def main():
     no_of_lines = int(input("How many quick picks? "))
     line_values = []
+
     for i in range(no_of_lines):
-        line = number_selection()
+        line = get_numbers()
         line_values.append(line)
+
     print(line_values)
 
 
-def number_selection() -> list:
+def get_numbers() -> list:
     """Generates list of unique random integers within a specified range. Returns list."""
     line = []
     for i in range(NUMBERS_PER_LINE):
-        not_unique = True
-        while not_unique:
+        unique = False
+        while not unique:
             number = randint(LOWEST_VALUE, HIGHEST_VALUE)
             if number not in line:
                 line.append(number)
-                not_unique = False
+                unique = True
     line.sort()
     return line
 
