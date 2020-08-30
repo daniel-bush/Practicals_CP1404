5cfd989977baf7726466bc9aad9331e9427a1e40
daniel.bush <Themepw999GH1987>
2020-08-30 14:58:34 +0100

    add dictionary, and input menu.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,7 +1,16 @@
 """hex_colours exercise."""
 
-HEX_COLOURS_DICT = {"aquamarine": "#7fffd4", "beige": "#f5f5dc"}
+HEX_COLOURS_DICT = {"aquamarine": "#7fffd4", "beige": "#f5f5dc", "brown": "#a52a2a",
+                    "blue": "#0000ff", "coral": "#ff7f50", "darkviolet": "#9400d3", "gold": "#ffd700",
+                    "gray": "#bebebe", "orange": "#ffa500", "orchid": "#da70d6"}
 
-print(HEX_COLOURS_DICT)
+colour = input("Colour: ").lower()
+while colour != "":
+    if colour in HEX_COLOURS_DICT:
+        print("The hex code for {} is {}.".format(colour, HEX_COLOURS_DICT[colour]))
+    else:
+        print("Incorrect colour!")
+    colour = input("Colour: ").lower()
+print("Thank you!")
 
-colour = input("Colour: ").upper()
+
