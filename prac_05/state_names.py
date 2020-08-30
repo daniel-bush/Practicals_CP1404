4552e0f79a3e2e56da63a85a44f7a0c8505d45f7
daniel.bush <Themepw999GH1987>
2020-08-30 14:19:54 +0100

    add loop with formatting. Exercise complete.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,14 +5,18 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+print(STATE_NAMES)
 
-state_code = input("Enter short state: ")
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+state = input("Enter short state: ").upper()
+while state != "":
+    if state in STATE_NAMES:
+        print(state, "is", STATE_NAMES[state])
     else:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state = input("Enter short state: ").upper()
+
+# loop with formatting
+for state in STATE_NAMES:
+    print("{:3} is {}".format(state, STATE_NAMES[state]))
