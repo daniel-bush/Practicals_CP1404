a59aa3ebded5c79fa22ba5b21f94b7360e55d32d
daniel.bush <Themepw999GH1987>
2020-09-10 15:42:46 +0100

    Add __str__ method

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,7 +1,19 @@
 """CP1404 Guitars! Exercise."""
 
+from datetime import datetime
+
+
 class Guitar:
+    """Class for storing details of a guitar."""
     def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar."""
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return string of guitar."""
+        return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)
+
+
+
