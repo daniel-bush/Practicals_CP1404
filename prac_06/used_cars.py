25ac08951f4e0c46c9fee2f6f12183c0416bf52c
daniel.bush <Themepw999GH1987>
2020-09-10 14:31:53 +0100

    Add question 1-5

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -8,12 +8,24 @@ def main():
     """Demo test code to show how to use car class."""
     my_car = Car(180)
     my_car.drive(30)
+
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
     print(my_car)
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+    # 1
+    limo = Car(100)
+    # 2
+    limo.add_fuel(20)
+    # 3
+    print("fuel =", limo.fuel)
+    # 4
+    limo.drive(115)
+    # 5
+    print("odo =", limo.odometer)
 
 
 main()
