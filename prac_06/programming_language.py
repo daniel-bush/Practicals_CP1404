764fdac7462540ee066cde662bac9a0c0375a7e7
daniel.bush <Themepw999GH1987>
2020-09-10 15:02:08 +0100

    Add __init__ method

---
+++ prac_06/programming_language.py
@@ -0,0 +1,14 @@
+"""CP1404 Exercise ProgrammingLanguage Class."""
+
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
+
+
