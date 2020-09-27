50ce11e1e97a0c7875015caddfc884bc524bd507
daniel.bush <Themepw999GH1987>
2020-09-27 19:59:36 +0100

    add handle_conversion function. Add features to kivy widget.

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -1,12 +1,21 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.app import StringProperty
 
+MILES_IN_KM = 1.60934
 
 class MilesToKMsApp(App):
+    message = StringProperty()
+
     def build(self):
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_conversion(self):
+        miles = int(self.root.ids.input_miles.text)
+        kms = miles * MILES_IN_KM
+        self.message = str(kms)
 
 
 MilesToKMsApp().run()
