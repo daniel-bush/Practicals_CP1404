bf66a07459e5198216b89a3f73882600d3e5a3c0
daniel.bush <Themepw999GH1987>
2020-10-01 10:40:29 +0100

    add finished to github

---
+++ prac_07/box_layout_demo.py
@@ -0,0 +1,24 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    """Boxlayout demo takes a name and greets them."""
+    def build(self):
+        """Build the kivy app from the kv file."""
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        """Displays on the widget the name."""
+        self.root.ids.output_label.text = "Hello {}".format(self.root.ids.input_name.text)
+
+    def handle_clear(self):
+        """Clears the widget."""
+        print("test")
+        self.root.ids.output_label.text = "Enter your name"
+        self.root.ids.input_name.text = ""
+
+
+BoxLayoutDemo().run()
