ba475afc8f1b0803790f4a86f8b40b0e93826ea6
daniel.bush <Themepw999GH1987>
2020-10-21 11:48:24 +0100

    add main function and menu

---
+++ prac_08/taxi_simulator.py
@@ -0,0 +1,18 @@
+"""Taxi simulator."""
+
+from prac_08.taxi import Taxi
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose, d)rive\n" \
+       ">>>"
+
+def main():
+    """This program simulates a taxi service company."""
+
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+
+    print(MENU)
+
+
+main()
+
