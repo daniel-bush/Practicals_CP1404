dd71a8acfdccba98904d91a931822070a8a4d473
daniel.bush <Themepw999GH1987>
2020-10-21 09:31:04 +0100

    add new Taxi

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,9 @@
+"""Tests for Class Taxi."""
+
+from prac_08.taxi import Taxi
+
+# 1 - Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km
+
+new_taxi = Taxi("Prius 1", 100, 1.23)
+print(new_taxi)
+
