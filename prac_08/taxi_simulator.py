57adca2fadbddf5dab1ad9eae4e5948a325b55a7
daniel.bush <Themepw999GH1987>
2020-10-21 11:57:55 +0100

    add menu to main

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -11,6 +11,16 @@ def main():
 
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
 
+    print("Let's drive!")
+    menu_choice = input(MENU).lower()
+    while menu_choice != "q":
+        if menu_choice == "c":
+            pass
+        elif menu_choice == "d":
+            pass
+        else:
+            print("invalid choice")
+        menu_choice = input(MENU).lower()
 
 
 
