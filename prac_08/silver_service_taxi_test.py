e95da5c44f067bbba6f30f426d6c16d5ef82ff20
daniel.bush <Themepw999GH1987>
2020-10-21 11:32:33 +0100

    add test fare

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -14,5 +14,16 @@ def main():
     print(less_fancy_taxi)
     print(normal_taxi)
 
+    # test flagfall
+
+    print(fancy_taxi.flagfall)
+
+    # test get_fare
+
+    fancy_taxi.drive(50)
+    expected_fare = (50*1.23*10+4.5)
+    actual_fare = fancy_taxi.get_fare()
+    print("expected fare = {}, actual fare = {}".format(expected_fare, actual_fare))
+
 
 main()
