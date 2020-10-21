cf0942e24e7b76a65d283a97b9fcd19a162a2d64
daniel.bush <Themepw999GH1987>
2020-10-21 09:46:51 +0100

    add code to main

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -2,34 +2,37 @@
 
 from prac_08.taxi import Taxi
 
-# 1 - Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km
 
-new_taxi = Taxi("Prius 1", 100, 1.23)
-print(new_taxi)
+def main():
+    # 1 - Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km
 
-# 2 - Drive the taxi 40km
-new_taxi.drive(40)
-print(new_taxi)
+    new_taxi = Taxi("Prius 1", 100, 1.23)
+    print(new_taxi)
 
-# 3 - Print the taxi's details and the current fare
+    # 2 - Drive the taxi 40km
+    new_taxi.drive(40)
+    print(new_taxi)
 
-print(new_taxi)
-print("name:", new_taxi.name)
-print("fuel", new_taxi.fuel)
-print("odometer", new_taxi.odometer)
-print("fare distance", new_taxi.current_fare_distance)
-print("fare $", new_taxi.price_per_km, "per/km")
-print("current fare: $", new_taxi.get_fare())
+    # 3 - Print the taxi's details and the current fare
 
-# 4 - Restart the meter (start a new fare) and then drive the car 100km
+    print(new_taxi)
+    print("name:", new_taxi.name)
+    print("fuel", new_taxi.fuel)
+    print("odometer", new_taxi.odometer)
+    print("fare distance", new_taxi.current_fare_distance)
+    print("fare $", new_taxi.price_per_km, "per/km")
+    print("current fare: $", new_taxi.get_fare())
 
-new_taxi.start_fare()
-# can only drive 60k due to fuel left
-new_taxi.drive(100)
-print(new_taxi)
+    # 4 - Restart the meter (start a new fare) and then drive the car 100km
 
-# 5 - Print the details and the current fare
-print(new_taxi)
-print("current fare: $", new_taxi.get_fare())
+    new_taxi.start_fare()
+    # can only drive 60k due to fuel left
+    new_taxi.drive(100)
+    print(new_taxi)
+
+    # 5 - Print the details and the current fare
+    print(new_taxi)
+    print("current fare: $", new_taxi.get_fare())
 
 
+main()
