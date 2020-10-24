b27ef80be08ed10f84fb8d73504574e3e8cabc87
daniel.bush <Themepw999GH1987>
2020-10-24 14:03:33 +0100

    finished

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -11,8 +11,13 @@ def main():
         if os.path.isdir(filename):
             continue
 
-
-
-
+        extension = filename.split('.')[-1]
+        if extension not in file_extension_to_category:
+            category = input("What category would you like to put {} files into? ".format(extension))
+            file_extension_to_category[extension] = category
+            try:
+                os.mkdir(category)
+            except FileExistsError:
+                pass
 
 main()
