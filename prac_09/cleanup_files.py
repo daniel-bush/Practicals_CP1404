d89680593abd946822486d6eab925907a588dd19
daniel.bush <Themepw999GH1987>
2020-10-24 12:07:43 +0100

    add replace code

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -5,28 +5,35 @@ import os
 def main():
     # change directory to 'Christmas'
     os.chdir('Lyrics/Christmas')
-    #
-    # # Print a list of all files in current directory
-    # print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
 
     # Loop through each file in the (current) directory
     for filename in os.listdir('.'):
-        # Ignore directories, just process files
+        # ignore directories
         if os.path.isdir(filename):
             continue
+        get_fixed_filename(filename)
 
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
+        # new_name = get_fixed_filename(filename)
+        # print("Renaming {} to {}".format(filename, new_name))
 
         # rename file to new name - in place
         # os.rename(filename, new_name)
 
 
-
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
+    print("Processing", filename)
+    # change " " to _ and .TXT to .txt
     new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-    return new_name
+    # indexed_name = enumerate(new_name)
+    # for i in indexed_name:
+    #     print
+
+
+
+
+    # new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    # return new_name
 
 
 
